8863a71dff6bb0bf5f098bf4a44750a50f9d64b7
yoonyoungyang <youngonly0118@gmail.com>
2025-09-30 09:04:57 +0900

    misson1 완성

---
+++ w2/chapter-2/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const handleIncreaseNumber = () => {
+    setCount(prev => prev + 1)
+    setCount(prev => prev + 1)
+    setCount(prev => prev + 1)
+    setCount(prev => prev + 1)
+    setCount(prev => prev + 1)
+    setCount(prev => prev + 1)
+  }
+
+  return (
+     <>
+      <h1>{count}</h1>
+      <button onClick={handleIncreaseNumber}>숫자 증가</button>
+     </>
+  )
+}
+
+export default App
